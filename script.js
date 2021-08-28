53d2f7e19842a551850776c70c1053c7c6e323f6
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2021-08-27 21:30:08 -0700

    Added

--- script.js
+++ script.js
@@ -7,65 +7,87 @@
  * 4. Add Previous/Next, First/Last and get Comic by ID functionality to the app
  * 5. Adjust UI states accordingly
  */
- class DomInterface {
+class DomInterface {
   constructor() {
-      this.form = document.querySelector('#comic-form');
-      this.searchField = document.querySelector('#search-input');
+    this.form = document.querySelector('#comic-form');
+    this.searchField = document.querySelector('#search-input');
 
-      this.title = document.querySelector('#comic-title');
-      this.image = document.querySelector('#comic-image');
+    this.title = document.querySelector('#comic-title');
+    this.image = document.querySelector('#comic-image');
 
-      this.error = document.querySelector('#error');
-      this.formError = document.querySelector('#form-error');
-      this.loader = document.querySelector('#loader');
+    this.error = document.querySelector('#error');
+    this.formError = document.querySelector('#form-error');
+    this.loader = document.querySelector('#loader');
 
-      this.controls = {
-          previous: document.querySelector('#request-prev'),
-          next: document.querySelector('#request-next'),
-          random: document.querySelector('#request-random'),
-          first: document.querySelector('#request-first'),
-          last: document.querySelector('#request-last'),
-      };
+    this.controls = {
+      previous: document.querySelector('#request-prev'),
+      next: document.querySelector('#request-next'),
+      random: document.querySelector('#request-random'),
+      first: document.querySelector('#request-first'),
+      last: document.querySelector('#request-last'),
+    };
   }
 
   clearResults() {
-      this.title.innerHTML = 'Loading...';
-      this.image.src = '';
-      this.image.alt = '';
+    this.title.innerHTML = 'Loading...';
+    this.image.src = '';
+    this.image.alt = '';
   }
 
   hideLoader() {
-      this.loader.classList.remove('d-flex');
-      this.loader.classList.add('d-none');
+    this.loader.classList.remove('d-flex');
+    this.loader.classList.add('d-none');
   }
 
   showLoader() {
-      this.loader.classList.remove('d-none');
-      this.loader.classList.add('d-flex');
+    this.loader.classList.remove('d-none');
+    this.loader.classList.add('d-flex');
   }
 
   showError() {
-      this.hideLoader();
-      this.error.innerHTML = 'There has been an error, please try again';
+    this.hideLoader();
+    this.error.innerHTML = 'There has been an error, please try again';
   }
 
   showFormError(message) {
-      this.hideLoader();
-      this.formError.innerHTML = message;
+    this.hideLoader();
+    this.formError.innerHTML = message;
   }
 
   hideErrors() {
-      this.error.innerHTML = '';
-      this.formError.innerHTML = '';
+    this.error.innerHTML = '';
+    this.formError.innerHTML = '';
   }
 
   showComics(data) {
-      const { title, img } = data;
+    const { title, img } = data;
 
-      this.title.innerHTML = title;
-      this.image.src = img;
-      if (data.alt) this.image.alt = data.alt;
+    this.title.innerHTML = title;
+    this.image.src = img;
+    if (data.alt) this.image.alt = data.alt;
 
-      this.hideLoader();
+    this.hideLoader();
   }
 }
+
+class RequestController {
+  constructor() {
+    this.DomInterface = new DOMInterface();
+    this.corsHeader = 'https://the-ultimate-api-challenge.herokuapp.com';
+    this.opiUrl = 'https://xkcd.com';
+    this.opiUrlFormat = 'info.0.json';
+    this.superAgent = superagent;
+
+    this.getCurrentComix();
+  }
+
+  getCurrentComix() {
+    const requestUrl = `${this.corsHeader}/${this.opiUrl}/${this.opiUrlFormat}`;
+
+    this.superAgent.get(requestUrl).end((error, responce) => {
+      console.log({ error, responce });
+    });
+  }
+}
+
+const comics = new RequestController();
