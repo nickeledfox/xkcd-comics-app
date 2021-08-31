eba41a8a808ce37e930cb0b8a3b8eb0e1d041820
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2021-08-30 21:00:23 -0700

    added theme toggle

--- soft_color.js
+++ soft_color.js
@@ -1,8 +1,10 @@
 const brightnessIcon = document.querySelector('.bi-brightness-low');
 const mainContainer = document.querySelector('.main__container');
-const container = document.querySelector('.container');
+const containers = document.querySelectorAll('.container');
 
 brightnessIcon.addEventListener('click', () => {
   mainContainer.classList.toggle('darker__bacground');
-  container.classList.toggle('paper__bacground');
+  containers.forEach((container) => {
+    container.classList.toggle('paper__bacground');
+  });
 });
